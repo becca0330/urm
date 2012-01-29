fbdde3e820d493521ea157d0a5240c09226dd71e
itm <internettechnik@gmx.at>
2012-01-29 11:14:07 +0100

    finally we try to use the provider domainfactory supported version of Rails 3.0.9 with ruby 1.8.7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,22 +50,23 @@ URM::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
+# 2012-01-29 switch to Rails 3.0.9 (only this is possible at domainfactory)
   # 2011-12-22 switch to Rails3.1
   # Compress both stylesheets and JavaScripts
-  config.assets.compress = false
+# config.assets.compress = false
   # Don't fallback to assets pipeline if a precompiled asset is missed
   # jf: try to fix error: add.png isn't precompiled
-  config.assets.compile = true
+#  config.assets.compile = true
   # Generate digests for assets URLs
-  config.assets.digest = false
+#  config.assets.digest = false
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
-  config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :scss
+#  config.assets.js_compressor  = :uglifier
+#  config.assets.css_compressor = :scss
   
   # 2012-01-18 try to fix error: "rake assets:precompile  fails undefined method `context_class' for nil:NilClass"
   # did not help: :(
-  config.assets.initialize_on_precompile = false
+#  config.assets.initialize_on_precompile = false
   
   
 end
