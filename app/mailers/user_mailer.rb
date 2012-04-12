d2ed3128abfadbcce91fbc30af02c7f52dce5f38
itm <internettechnik@gmx.at>
2012-04-12 21:09:19 +0200

    sending mail via sendmail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,11 +4,14 @@ class UserMailer < ActionMailer::Base
   include ActionController::UrlWriter
   
   def confirmation_code(user)
-      from "URM-Admin <urm@feiner.at>"
-      recipients user.email
-      subject 'Please confirm your registration'
-      body :user => user
-      sent_on Time.now
+      #from "URM-Admin <urm@feiner.at>"
+      #recipients user.email
+      #subject 'Please confirm your registration'
+      #body :user => user
+      #sent_on Time.nowA
+      @user=user
+      mail( :to       => "johannes.feiner@fh-joanneum.at #{user.email}", 
+            :subject  => "Please confirm your registration")
   end
   
 end
--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -2,6 +2,7 @@ class UserObserver < ActiveRecord::Observer
   observe :user
 
   def after_create(user)
+    puts("we send the confirmation code to user #{user.email} (pending=#{user.pending?})")
     UserMailer.deliver_confirmation_code(user) if user.pending?
   end
 end
